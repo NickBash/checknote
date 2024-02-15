f998bb6d9350f05f98601d11f28e88c68e1eb08a
Nikita Bashkankov <nikitabash7@gmail.com>
2024-02-15 21:51:28 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'CheckNote',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="checknote-theme-2"
+          >
+            <Toaster position="bottom-center" />
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://grateful-killdeer-99.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
