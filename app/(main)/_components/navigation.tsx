99242a5108c1c0d55689ad988b45e744c5fe83b5
Nikita Bashkankov <nikitabash7@gmail.com>
2024-02-16 00:15:19 +0500

    search command create

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
 import { useMutation } from 'convex/react';
@@ -10,6 +15,7 @@ import {
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
@@ -17,6 +23,7 @@ import { toast } from 'sonner';
 import { useMediaQuery } from 'usehooks-ts';
 import { DocumentList } from './document-list';
 import { Item } from './item';
+import { TrashBox } from './trash-box';
 import { UserItem } from './user-item';
 
 const Navigation = () => {
@@ -162,6 +169,20 @@ const Navigation = () => {
             icon={Plus}
             label="Add a page"
           />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item
+                label="Trash"
+                icon={Trash}
+              />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? 'bottom' : 'right'}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
